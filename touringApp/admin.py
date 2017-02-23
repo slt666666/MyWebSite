4c0ea42fd188f70b3aaf8f4c1865ce1b386397c1
堺俊之 <toshi6661024@gmail.com>
2017-02-23 13:54:32 +0900

    adminを整理

--- touringApp/admin.py
+++ touringApp/admin.py
@@ -3,5 +3,19 @@ from django.contrib import admin
 from .models import Route, Pass
 # Register your models here.
 
-admin.site.register(Route)
-admin.site.register(Pass)
+class PassInline(admin.TabularInline):
+    model = Pass
+    extra = 1
+
+
+class RouteAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {'fields':['route_name']}),
+        ('Date information', {'fields':['log_date']}),
+    ]
+    inlines = [PassInline]
+    list_filter = ['log_date']
+    list_display = ('route_name', 'log_date')
+
+
+admin.site.register(Route, RouteAdmin)
